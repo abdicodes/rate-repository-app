417c7e4a9a1383d1e22e1609b99ced888e5a7827
Abdirahman Suldaan <suldaan.89@hotmail.com>
2022-10-25 18:36:34 +0300

    to k suffex helper function

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,12 +1,17 @@
 import { View, Text } from 'react-native';
 
+function kFormatter(num) {
+  return Math.abs(num) > 999
+    ? Math.sign(num) * (Math.abs(num) / 1000).toFixed(1) + 'k'
+    : Math.sign(num) * Math.abs(num);
+}
 const RepositoryItem = ({ item }) => (
   <View>
     <Text>Full Name: {item.fullName}</Text>
     <Text>description: {item.description}</Text>
     <Text>language: {item.language}</Text>
-    <Text>Stars: {item.stargazersCount}</Text>
-    <Text>forks: {item.forksCount}</Text>
+    <Text>Stars: {kFormatter(item.stargazersCount)}</Text>
+    <Text>forks: {kFormatter(item.forksCount)}</Text>
     <Text>reviews: {item.reviewCount}</Text>
     <Text>rating: {item.ratingAverage}</Text>
   </View>

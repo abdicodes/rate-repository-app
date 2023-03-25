3d0d1bdce6e07c22b98ce288af0038ed9466a1cd
Abdirahman Suldaan <suldaan.89@hotmail.com>
2023-03-26 00:23:08 +0200

    sorting implemented

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,16 +1,47 @@
 import { useQuery } from '@apollo/client';
 import { GET_REPOSITORIES } from '../graphql/queries';
 import RepositoryListContainer from './RepositoryListContainer';
+import { useState } from 'react';
 
 const RepositoryList = () => {
+  const [selectedOrderType, setSelectedOrderType] = useState('CREATED_AT');
+  const [selectDirection, setSelectDirection] = useState('DESC');
+
   const { data, loading } = useQuery(GET_REPOSITORIES, {
     //to avoid caching issues.
     fetchPolicy: 'cache-and-network',
+    variables: {
+      orderBy: selectedOrderType,
+      orderDirection: selectDirection,
+    },
   });
   if (loading) return null;
   const { repositories } = data;
 
-  return !loading && <RepositoryListContainer repositories={repositories} />;
+  const onSubmit = (itemValue) => {
+    if (itemValue === 'latest') {
+      setSelectedOrderType('CREATED_AT');
+      setSelectDirection('DESC');
+    }
+
+    if (itemValue === 'highRate') {
+      setSelectedOrderType('RATING_AVERAGE');
+      setSelectDirection('DESC');
+    }
+    if (itemValue === 'lowRate') {
+      setSelectedOrderType('RATING_AVERAGE');
+      setSelectDirection('ASC');
+    }
+  };
+
+  return (
+    !loading && (
+      <RepositoryListContainer
+        repositories={repositories}
+        onSubmit={onSubmit}
+      />
+    )
+  );
 };
 
 export default RepositoryList;
--- src/components/RepositoryListContainer.jsx
+++ src/components/RepositoryListContainer.jsx
@@ -1,27 +1,108 @@
-import { FlatList, View, StyleSheet } from 'react-native';
+import {
+  FlatList,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+} from 'react-native';
 import theme from '../theme';
 import RepositoryItem from './RepositoryItem';
+import { Picker } from '@react-native-picker/picker';
+import { useState } from 'react';
 
 const styles = StyleSheet.create({
   separator: {
     height: 10,
     backgroundColor: theme.colors.separator,
   },
+  container: {
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  button: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: '80%',
+    padding: 10,
+    borderWidth: 1,
+    borderColor: '#bbb',
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#444',
+  },
+  selectedValue: {
+    fontSize: 16,
+    color: '#666',
+    marginLeft: 5,
+  },
+  arrowIcon: {
+    width: 0,
+    height: 0,
+    borderLeftWidth: 5,
+    borderRightWidth: 5,
+    borderTopWidth: 8,
+    borderLeftColor: 'transparent',
+    borderRightColor: 'transparent',
+    borderTopColor: '#666',
+  },
+  picker: {
+    width: '80%',
+    borderWidth: 1,
+    borderColor: '#bbb',
+    borderRadius: 5,
+    backgroundColor: '#fff',
+  },
 });
 
 const ItemSeparator = () => <View style={styles.separator} />;
 
-export const RepositoryListContainer = ({ repositories }) => {
+export const RepositoryListContainer = ({ repositories, onSubmit }) => {
   const renderItem = ({ item }) => <RepositoryItem item={item} />;
 
   const repositoryNodes = repositories.edges.map((edge) => edge.node);
+  const valueChangeHandler = (itemValue) => {
+    onSubmit(itemValue);
+  };
 
+  const SortingProps = () => {
+    const [showPicker, setShowPicker] = useState(false);
+
+    const handlePickerButtonPress = () => {
+      setShowPicker(!showPicker);
+    };
+
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={handlePickerButtonPress}
+        >
+          <Text style={styles.buttonText}>Sort by</Text>
+          <View style={styles.arrowIcon} />
+        </TouchableOpacity>
+
+        {showPicker && (
+          <Picker style={styles.picker} onValueChange={valueChangeHandler}>
+            <Picker.Item label="latest repositories" value="latest" />
+            <Picker.Item label="Highest rated repositories" value="highRate" />
+            <Picker.Item label="Lowest rated repositories" value="lowRate" />
+          </Picker>
+        )}
+      </View>
+    );
+  };
   return (
     <FlatList
       data={repositoryNodes}
       renderItem={renderItem}
       keyExtractor={(item) => item.id}
       ItemSeparatorComponent={ItemSeparator}
+      ListHeaderComponent={SortingProps}
     />
   );
 };
